be793a73cd026ae96e623058b4bbe6b0dcfeef00
Melanie Reisenbichler <mreisenbichler.mmt-b2016@fh-salzburg.ac.at>
2018-01-27 17:26:23 +0100

    add messages to validations

--- logging/app/models/user.rb
+++ logging/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     #mind. 1 Zahl
     
     validates :password, length: { minimum: 8 } # min. length
-    validates :password, format: {:with => /\A(?=.*\d)/x} # must contain number/digit
-    validates :password, format: {:with => /\A(?=.*[[:^alnum:]])/x} # must contain symbol
+    validates :password, format: {:with => /\A(?=.*\d)/x, :message => "Invalid! Password must contain a number."} # must contain number/digit
+    validates :password, format: {:with => /\A(?=.*[[:^alnum:]])/x,:message => "Invalid! Password must contain a symbol."} # must contain symbol
 end
