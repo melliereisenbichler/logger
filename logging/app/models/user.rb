e956bcd198323a04d42c8f52258be057ea82658e
Melanie Reisenbichler <mreisenbichler.mmt-b2016@fh-salzburg.ac.at>
2018-01-27 17:22:34 +0100

    add validations for password

--- logging/app/models/user.rb
+++ logging/app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     #mind. 1 Sonderzeichen
     #mind. 1 Zahl
     
-    validates :password, length: { minimum: 8 }
+    validates :password, length: { minimum: 8 } # min. length
+    validates :password, format: {:with => /\A(?=.*\d)/x} # must contain number/digit
+    validates :password, format: {:with => /\A(?=.*[[:^alnum:]])/x} # must contain symbol
 end
